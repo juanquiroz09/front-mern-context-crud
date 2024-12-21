4d2fbe7675504ddb1684288616941bf78502dd13
[juanq] <[juan.quiroz@gruposaleta.com]>
2024-12-21 09:04:42 -0500

    update home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,22 +4,17 @@ import { PostCard } from "../components/PostCard";
 import { VscEmptyWindow } from "react-icons/vsc";
 
 export function HomePage() {
-  const { posts, loading } = usePosts();
+  const { posts } = usePosts();
 
   const renderPost = () => {
-    if (loading) {
-      return <div>Loading...</div>; // Puedes reemplazar esto con un spinner o algún otro indicador
-    }
-  
-    if (!Array.isArray(posts) || posts.length === 0) {
+    if (posts.length === 0)
       return (
         <div className="flex flex-col justify-center items-center">
           <VscEmptyWindow className="w-48 h-48 text-white" />
           <h1 className="text-white text-2xl">There are no posts</h1>
         </div>
       );
-    }
-  
+
     return (
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {posts.map((post) => (
